22f11c2e9cf7383c36b87e75658138a5cee010ad
NeutrinoZh <neutrinozh@gmail.com>
2023-05-20 12:33:33 +0300

    change gun direction delay

--- Assets/EvilSpaceDefense/Scripts/Earth/EarthController.cs
+++ Assets/EvilSpaceDefense/Scripts/Earth/EarthController.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using System.Collections;
 
 namespace EvilSpaceDefense {
     [RequireComponent(typeof(EarthView))]
     public class EarthController : MonoBehaviour {
+
         private void Awake() {
             //==========================================//
             m_bullet_spawner = new BulletSpawner(m_model.bullet_prefab);
@@ -21,22 +23,42 @@ namespace EvilSpaceDefense {
             //==========================================//
         }
 
+        //==========================================//
+
         [SerializeField]
         private EarthModel m_model;
         private EarthView m_view;
 
+        //==========================================//
+
         private BulletSpawner m_bullet_spawner;
 
-        private void Update() { 
-            m_view.Rotate();
-        }
+        //==========================================//
+
+        const float DELAY_BEFORE_CHANGE_DIRECTION = 0.2f;
+
+        //==========================================//
 
         private void SpawnBullet() {
             m_bullet_spawner.SpawnBullet(m_model.gun.position, transform.right);
         }
 
         private void ChangeDirection()  {
-            m_model.direction = -m_model.direction;
+            StartCoroutine(ChangeDirectionByDelay(-m_model.direction));
+            m_model.direction = 0;
+        }
+
+        //==========================================//
+
+        private IEnumerator ChangeDirectionByDelay(int _new_direction) {
+            yield return new WaitForSeconds(DELAY_BEFORE_CHANGE_DIRECTION);
+            m_model.direction = _new_direction;
+        }
+
+        //==========================================//
+        
+        private void Update() { 
+            m_view.Rotate();
         }
     }
 }
